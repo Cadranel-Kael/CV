c9c3ce3b105a43960b85abb2a10203e24f36a700
kael-cadranel <kael.cadranel00@gmail.com>
2024-02-22 14:56:39 +0100

    Add scroll slide

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,10 @@
 import { contactForm } from "./contactForm.js";
 import { burgerMenu } from "./burgerMenu.js";
+import { slideScroll } from "./slideScroll.js";
 function main() {
     contactForm();
     burgerMenu();
+    slideScroll('carousel', 0.1);
 }
 main();
 //# sourceMappingURL=main.js.map
